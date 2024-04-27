86a52f5d2ef8464e6917a5a14d5ce3a590090dab
exam <exam@202STUD100>
2024-04-27 11:04:30 +0530

    Add project files.

---
+++ Simple_Interest.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Simple_Interest.asmx.cs" Class="SimpleInterest_Calculator.Simple_Interest" %>
---
+++ Simple_Interest.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SimpleInterest_Calculator
+{
+    /// <summary>
+    /// Summary description for Simple_Interest
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Simple_Interest : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public float Calculator(float p,float n,float r)
+        {
+            return (p*n*r)/100;   
+        }
+    }
+}
